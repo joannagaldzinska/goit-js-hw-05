c08448e86ae029368409db53a4da2c342ad3349c
Joanna Galdzinska <joanna.galdzinska@gmail.com>
2020-10-26 18:55:54 +0200

    DONE

--- task-5.js
+++ task-5.js
@@ -22,23 +22,44 @@ class Car {
          this._price = value;
      }
 
-     turnOn(isOn) {
+     turnOn() {
          this.isOn = true;
      }
-     turnOff(isOn) {
+
+     turnOff() {
          this.isOn = false
+         this.speed = 0
      }
+
      accelerate(value) {
-         if (value > 0) return this.speed 
+         if (this.speed + value > this._maxSpeed) {
+         this.speed = this._maxSpeed}
+         else if (value +this.speed <= 0)
+         {this.speed = 0}
+         else 
+         {
+         this.speed += value}
+         
+      }
+
+      decelerate(value) {
+        if (this.speed - value < 0) 
+        { this.speed = 0}
+       else 
+       { this.speed -= value;}
      }
-     decelerate(value) {}
-     drive(hours) {}
+
+     drive(hours) {
+         if (this.isOn === true ) {
+             return this.distance += hours * this.speed;
+         }
+     }
    }
     
    const mustang = new Car({ maxSpeed: 200, price: 2000 });
-
+console.log(mustang)
    mustang.turnOn();
-    mustang.accelerate(50);
+    mustang.accelerate(1000);
    mustang.drive(2);
 console.log(Car.getSpecs(mustang));
    // 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
@@ -47,10 +68,10 @@ mustang.decelerate(20);
 mustang.drive(1);
 mustang.turnOff();
    
-console.log(Car.getSpecs(mustang));
+//console.log(Car.getSpecs(mustang));
    // 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
    
- console.log(mustang.price); // 2000
+ //console.log(mustang.price); // 2000
  mustang.price = 4000;
-console.log(mustang.price); // 4000
+//console.log(mustang.price); // 4000
    
